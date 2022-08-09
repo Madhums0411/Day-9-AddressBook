39c25ef5b930cc3f2c60daeded1ec3b92c1dcac1
madhums0411 <madhums41175@gmail.com>
2022-08-09 21:59:47 +0530

    Adress book has Unique name

---
+++ AddressBook/AddressBook/AddressBookSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    public class AddressBookSystem
+    {
+        public Dictionary<string, AddressBook> addressBooks = new Dictionary<string, AddressBook>();
+        public void AddAddressBook()
+        {
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddMultipleContact();
+            Console.WriteLine("enter the addressbook name");
+            string addressName = Convert.ToString(Console.ReadLine());
+            addressBooks.Add(addressName.ToLower(), addressBook);
+
+        }
+    }
+}
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -2,32 +2,72 @@
 
 Console.WriteLine("Welcome to Address Book problem");
 
-AddressBook address = new AddressBook();
-Console.WriteLine();
-string command = "";
+AddressBookSystem addressBookSystem = new AddressBookSystem();
+string command1 = "";
 
-while (command != "exit")
-{
-    Console.Clear();
-    Console.WriteLine("Please enter a command add,edit,remove,display,multipleperson: ");
-    command = Console.ReadLine().ToLower();
 
-    switch (command)
+    while (command1 != "exit")
     {
-        case "add":
-            address.AddPerson();
-            break;
-        case "edit":
-            address.EditContact();
-            break;
-        case "remove":
-            address.RemoveContact();
-            break;
-        case "display":
-            address.Display();
-            break;
-        case "multipleperson":
-            address.AddMultipleContact();
-            break;
+        Console.WriteLine("enter the option: 1.Add AdressBook 2.Edit AddressBook 3.Remove AddressBook");
+        int option = Convert.ToInt32(Console.ReadLine());
+
+        switch (option)
+        {
+            case 1:
+            addressBookSystem.AddAddressBook();
+                break;
+            case 2:
+                Console.WriteLine("enter the addressbook name you want to edit");
+                string addressBookName = Console.ReadLine();
+
+
+                if (addressBookSystem.addressBooks.ContainsKey(addressBookName.ToLower()))
+                {
+
+
+                    string command = "";
+
+                    while (command != "exit")
+                    {
+
+                        Console.WriteLine("Please enter a command add,edit,remove,display,multipleperson: ");
+                        command = Console.ReadLine().ToLower();
+
+
+                        switch (command)
+                        {
+                            case "add":
+
+
+                                addressBookSystem.addressBooks[addressBookName.ToLower()].AddPerson();
+                                break;
+                            case "edit":
+                                addressBookSystem.addressBooks[addressBookName.ToLower()].EditContact();
+                                break;
+                            case "remove":
+                                addressBookSystem.addressBooks[addressBookName.ToLower()].RemoveContact();
+                                break;
+                            case "display":
+                                addressBookSystem.addressBooks[addressBookName.ToLower()].Display();
+                                break;
+                            case "multipleperson":
+                                addressBookSystem.addressBooks[addressBookName.ToLower()].AddMultipleContact();
+                                break;
+                            default:
+                                command = "exit";
+                                break;
+                        }
+                    }
+                }
+                break;
+            default:
+                command1 = "exit";
+                break;
+
+
+
+
+
+        }
+
     }
-}
